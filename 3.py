c095d917f00d57446ba243ae98fa9f9fa26e3d9d
Jawad Mughal <73652620+JawadMughalJD@users.noreply.github.com>
2020-12-10 18:37:18 +0500

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,15 @@
+import time
+
+def Song(lyrics):
+    for i in range (0,n):
+        print(lyrics[i])
+        time.sleep(1.5)
+        
+    
+lyrics = ("Nazrein chura ke mujhse Jaa to rahe ho,Par aaine se kaise Nazrein churaoge,Zinda rahunga main Ehsaas mein hardum,Daava hai mera Tum na mujhe bhool paoge,Jab zikar hoga ishq ka To ro hi padoge,Afsos karoge afsos karoge,Likh ke leejiyega ek roz karoge,Afsos karoge afsos karoge,Likh ke leejiyega ek roz karoge")
+
+a = lyrics.split(',')
+
+n = len(a)
+
+Song(a)
